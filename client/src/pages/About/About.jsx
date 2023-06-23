7e615f8d07172db8c1bddbce09b55bb77e377c73
Aryse Tansy <medictansy@gmail.com>
2023-06-23 13:34:27 -0400

    Fix terminal event listener

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import { more, prompt } from "./Data/data";
 
+let hasSelected = false;
+
 const About = () => {
-  const [inputVal, setInputVal] = useState("");
   const [showMore, setShowMore] = useState(false);
   const [showInvalid, setShowInvalid] = useState(false);
   const [showQuit, setShowQuit] = useState(false);
-  const [hasSelected, setHasSelected] = useState(false);
 
   const handleKeyDown = (e) => {
     if (!hasSelected) {
       e.key === "y" && setShowMore(true);
       e.key === "n" && setShowQuit(true);
       e.key !== "y" && e.key !== "n" && setShowInvalid(true);
-      setHasSelected(true);
+      hasSelected = true;
     }
   };
 
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
     <section
-      onKeyDown={handleKeyDown}
       cascade
       className=" overflow-auto w-full h-full bg-slate-950 text-slate-50"
     >
@@ -34,14 +37,6 @@ const About = () => {
           </>
         );
       })}
-
-      <input
-        autoFocus
-        className="text-transparent cursor-none bg-transparent outline-none"
-        value={inputVal}
-        onChange={(e) => setInputVal(e.target)}
-        type="text"
-      />
       {showQuit ? <p>_kw_: .quitting</p> : null}
       {showInvalid ? <p>_kw_: INVALID ENTRY | .quitting</p> : null}
       {showMore ? (
