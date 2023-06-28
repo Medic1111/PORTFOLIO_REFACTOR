a254e6ef179e158b71f8edca51d36d7f1745e28c
Aryse Tansy <medictansy@gmail.com>
2023-06-28 08:06:46 -0400

    Isolate projects array

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,13 +1,7 @@
 import { useContext } from "react";
 import { uiCtx } from "../../features/ui-ctx";
+import { data } from "./data/data";
 
-const titles = [
-  "M11-Studio",
-  "Nomad-me",
-  // "Fleet-control",
-  // "!Cube-Studio",
-  // "Open-source",
-];
 const Projects = () => {
   const uiMgr = useContext(uiCtx);
 
@@ -21,13 +15,16 @@ const Projects = () => {
   return (
     <section className="overflow-auto flex  flex-col items-start justify-start gap-8  w-full h-full ">
       <div className=" w-full h-full flex flex-wrap flex-col items-start justify-start  p-5">
-        {titles.map((el) => {
+        {data.map((el) => {
           return (
             <div className="w-1/3 sm:w-1/5 flex flex-col items-center">
-              <button onClick={() => selectProject(el)} className="  text-5xl">
+              <button
+                onClick={() => selectProject(el.title)}
+                className="  text-5xl"
+              >
                 🗂
               </button>
-              <p className="pb-5 text-center">{el}</p>
+              <p className="pb-5 text-center">{el.title}</p>
             </div>
           );
         })}
