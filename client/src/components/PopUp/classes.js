9d57bfe20a6ebfb40b614f6dae0276a8a101fd41
Aryse Tansy <medictansy@gmail.com>
2023-07-09 08:57:59 -0400

    Border pop up

--- client/src/components/PopUp/classes.js
+++ client/src/components/PopUp/classes.js
@@ -1,6 +1,6 @@
 export const classes = {
   popUp:
-    "p-1 gap-5 flex flex-col items-center bg-darker  w-[80%] h-[75%]  sm:h-4/5  sm:w-2/3   border-2 border-borderGray  sm:mr-5 gap-1",
+    "p-1 gap-5 flex flex-col items-center bg-darker  w-[80%] h-[75%]  sm:h-4/5  sm:w-2/3   border-2    border-slate-950 border-2  border-t-slate-300 border-l-slate-300  sm:mr-5 gap-1",
   headerBox:
     " bg-light border-b-2 border-borderGray py-1   flex  w-full items-center bg-btnGray justify-between",
   icon: "h-full flex items-center gap-1 pl-1 font-mono uppercase",
